8303b715559636ea61d109bda34d3f9813f08fe3
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-09-09 06:28:24 +0300

    feat: update Installation page header and subheader for clarity

--- src/app/learn/installation/page.tsx
+++ src/app/learn/installation/page.tsx
@@ -5,7 +5,8 @@ interface IProps {
 const page = ({}: IProps) => {
   return (
     <div>
-      <h1>Installation Page(/)(page.tsx)</h1>
+      <h1>Learn Page</h1>
+      <h2>Thinking in React(/learn/installation)(page.tsx)</h2>
     </div>
   )
 }
