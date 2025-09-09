26ae21a873d0a8e1011fdff0246ad28c06eec0da
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-09-09 06:28:51 +0300

    feat: add Thinking in React page component with initial structure (nested route thinking-in-react folder in learn folder)

---
+++ src/app/learn/thinking-in-react/page.tsx
@@ -0,0 +1,14 @@
+interface IProps {
+
+}
+
+const page = ({}: IProps) => {
+  return (
+    <div>
+      <h1>Learn Page</h1>
+      <h2>Thinking in React(/learn/thinking-in-react)(page.tsx)</h2>
+    </div>
+  )
+}
+
+export default page
