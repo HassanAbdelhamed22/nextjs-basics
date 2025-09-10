fde950c63dcda2de6ef8102fd85ae9a21b654454
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-09-10 07:29:11 +0300

    feat: add loading component for product pages with loading message

---
+++ src/app/products/[id]/loading.tsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <main className="min-h-screen bg-gray-50 dark:bg-gray-900 py-10 px-6 flex flex-col items-center transition-colors duration-300">
+        <p>Loading...</p>
+    </main>
+  );
+};
+
+export default loading;
