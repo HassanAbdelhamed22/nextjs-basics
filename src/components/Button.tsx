76fe9fc0a44a2e4bf4114d757efe4a8424d31fe0
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-09-10 15:45:12 +0300

    feat: update Button component to remove onClick prop and log click event (make it client component)

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,15 @@
+"use client";
+
 interface IProps {
   text: string;
-  onClick?: () => void;
+  // onClick?: () => void;
 }
 
-const Button = ({ text, onClick }: IProps) => {
+const Button = ({ text }: IProps) => {
   return (
     <button
       className="w-full bg-blue-600 hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600 text-white font-semibold py-3 rounded-xl transition"
-      onClick={onClick}
+      onClick={() => console.log("Clicked")}
     >
       {text}
     </button>
